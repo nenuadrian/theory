8f5130ceae8939c3d0223ea0876aca23f5a29ade
Adrian Mircea Nenu <adrian.nenu@gmail.com>
2026-05-31 18:39:03 +0100

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,8 @@ import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://nenuadrian.github.io',
+	base: '/theory/',
 	markdown: {
 		remarkPlugins: [remarkGfm, remarkMath],
 		rehypePlugins: [rehypeKatex],
